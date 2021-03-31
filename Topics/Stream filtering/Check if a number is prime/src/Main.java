f3f4b7a1e3564e01c6d929a63c8c475db7c4006e
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2021-03-31 16:14:52 +0300

    add problem Check if a number is prime

---
+++ Topics/Stream filtering/Check if a number is prime/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.stream.*;
+
+class PrimeNumbers {
+
+    /**
+     * Checking if a number is prime
+     *
+     * @param number to test >= 2
+     * @return true if number is prime else false
+     */
+    private static boolean isPrime(long number) {
+        long l = LongStream
+                .rangeClosed(1, number)
+                .filter(x -> number % x == 0)
+                .count();
+        return l <= 2;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine().trim();
+
+        int n = Integer.parseInt(line);
+
+        System.out.println(isPrime(n) ? "True" : "False");
+    }
+}
